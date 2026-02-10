141349ca9cfd6077eb9f9a8a7961d10ea1b1370d
Mukul Kohli <2020010002884@gnducjal.org>
2026-02-10 07:05:47 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+name = "Mukul Kohli"
+age = 24
+area = 65.6
+
+print('Data type of name is: ', type(name))
+print('Data type of age is: ', type(age))
+print('Data type of area is: ', type(area))
---
+++ practice1.py
@@ -0,0 +1,6 @@
+# program to take a age as input and print value entered and its data type
+
+age = int(input("Enter your age: "))
+
+print("You entered: ", age)
+print("Data type of age is: ", type(age))
