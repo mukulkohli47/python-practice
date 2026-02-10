e2893727e6ea920fe06992801d51ba71034cf821
Mukul Kohli <2020010002884@gnducjal.org>
2026-02-10 06:56:59 +0530

    add assigments

---
+++ assignment1.py
@@ -0,0 +1,3 @@
+# print your name 10 times using asetric symbols
+
+print('Mukul Kohli\n' * 10)
---
+++ assignment2.py
@@ -0,0 +1,6 @@
+# calculate area of a circle get input from user
+
+d = int(input("Enter the diameter of the circle: "))
+r = d/2
+area = 3.14 * (r**2)
+print("The area of the circle is: ", area)
